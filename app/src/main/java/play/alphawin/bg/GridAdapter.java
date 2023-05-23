1f0e6267056c26f33ae623a33504300fbd07c828
USER <nikison1804@mail.ru>
2023-05-24 00:47:59 +0300

    Initial commit

---
+++ app/src/main/java/play/alphawin/bg/GridAdapter.java
@@ -0,0 +1,46 @@
+package play.alphawin.bg;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class GridAdapter extends BaseAdapter {
+    private Context context;
+    final int[] pics = new int[]{R.drawable.sport1, R.drawable.sport2,
+            R.drawable.sport3, R.drawable.sport4,
+            R.drawable.sport5, R.drawable.sport6};
+    public GridAdapter(Context context){
+        this.context=context;
+    }
+    @Override
+    public int getCount() {
+        return 6;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ImageView imageView;
+        if(view==null){
+            imageView=new ImageView(context);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(350,350));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+        else imageView =(ImageView)view;
+        imageView.setImageResource(R.drawable.bg);
+
+        return imageView;
+    }
+}
---
+++ app/src/main/java/play/alphawin/bg/MainActivity2.java
@@ -0,0 +1,185 @@
+package play.alphawin.bg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private ImageView mImageView = null;
+    public int level = 1;
+    private boolean win = false;
+    private int contPair=0;
+    private int pairs = 3;
+    final int[] pics = new int[]{R.drawable.sport1, R.drawable.sport2,
+            R.drawable.sport3, R.drawable.sport4,
+            R.drawable.sport5, R.drawable.sport6};
+    int[] pos ={0,1,2,0,1,2};
+    int currpos = -1;
+    public static Animation mAppearAnimation, mFadeAnimation;
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mAppearAnimation = AnimationUtils.loadAnimation(this, R.anim.appear);
+        mFadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade);
+        mAppearAnimation.setAnimationListener(animationAppearListener);
+        mFadeAnimation.setAnimationListener(animationFadeListener);
+        //adapter = new ArrayAdapter<String>(this, R.layout.item_layout,R.id.tvFill, data);
+        gvMain = (GridView)findViewById(R.id.gvMain);
+        GridAdapter gridAdapter = new GridAdapter(this);
+        gvMain.setAdapter(gridAdapter);
+        gvMain.setEnabled(false);
+        gvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(currpos<0){
+                    currpos=i;
+                    mImageView=(ImageView)view;
+                    ((ImageView)view).setImageResource(pics[pos[i]]);
+                    mImageView.startAnimation(mAppearAnimation);
+                    win = false;
+                }
+                else{
+                    if(currpos==i){
+                        mImageView.startAnimation(mFadeAnimation);
+                    }
+                    else if(pos[currpos]!=pos[i]){
+                        mImageView.startAnimation(mFadeAnimation);
+                        mImageView.setImageResource(pics[pos[currpos]]);
+                    }
+                    else{
+                        win = true;
+                        ((ImageView)view).setImageResource(pics[pos[i]]);
+                        contPair++;
+                        if(contPair==pairs){
+                            Dialog dialogWin = new Dialog(MainActivity2.this);
+                            dialogWin.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                            dialogWin.setContentView(R.layout.win_dialog);
+                            dialogWin.setCancelable(false);
+                            dialogWin.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                            dialogWin.show();
+                            TextView text_dialog = (TextView) dialogWin.findViewById(R.id.dialog_reminder);
+                            Button btn_okey= (Button) dialogWin.findViewById(R.id.ok_btn);
+                            btn_okey.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    dialogWin.dismiss();
+                                }
+                            });
+                        }
+
+                    }
+                    currpos=-1;
+                }
+            }
+        });
+        adjustGridView();
+    }
+
+
+    private void adjustGridView(){
+        gvMain.setNumColumns(2);
+        gvMain.setColumnWidth(300);
+        gvMain.setVerticalSpacing(20);
+        gvMain.setHorizontalSpacing(20);
+        gvMain.setStretchMode(GridView.STRETCH_SPACING_UNIFORM);
+    }
+
+    Animation.AnimationListener animationAppearListener = new Animation.AnimationListener() {
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            if(win==false) mImageView.startAnimation(mFadeAnimation);
+            currpos = -1;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+    };
+
+    Animation.AnimationListener animationFadeListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            mImageView.setImageResource(R.drawable.bg);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            // TODO Auto-generated method stub
+        }
+    };
+
+    public void start(View view) {
+        gvMain.setEnabled(true);
+    }
+
+    public void changeLevel(View view) {
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_layout);
+        dialog.setCancelable(false);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+        TextView text_dialog = (TextView) dialog.findViewById(R.id.dialog_reminder);
+        Button btn_1= (Button) dialog.findViewById(R.id.lvl1);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                level=1;
+                dialog.dismiss();
+            }
+        });
+        Button btn_2= (Button) dialog.findViewById(R.id.lvl2);
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                level=2;
+                dialog.dismiss();
+            }
+        });
+        Button btn_3= (Button) dialog.findViewById(R.id.lvl3);
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                level=3;
+                dialog.dismiss();
+            }
+        });
+        Button btn_4= (Button) dialog.findViewById(R.id.lvl4);
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                level=4;
+                dialog.dismiss();
+            }
+        });
+    }
+}
